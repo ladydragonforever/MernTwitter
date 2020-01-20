a4dfd4a6365c893365b035aa21b2ddf28dedffbc
Dan <lydia.danlee@gmail.com>
2020-01-20 00:31:57 -0800

    log

--- app.js
+++ app.js
@@ -9,13 +9,22 @@ const passport = require('passport');
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const path = require('path');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!!"));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+} else {
+  app.get("/", (req, res) => res.send("Hello World!!"));
+}
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
@@ -28,3 +37,4 @@ app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
