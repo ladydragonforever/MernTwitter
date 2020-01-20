bd65243cee721656480d333aae7048bc9d7821e9
Dan <lydia.danlee@gmail.com>
2020-01-20 00:21:45 -0800

    heroku app

---
+++ frontend/build/precache-manifest.3933ee9b77026828f50f90c82ea6a8b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "038f835aeacaa8d1e6f1",
+    "url": "/static/css/main.7bbf4fa4.chunk.css"
+  },
+  {
+    "revision": "038f835aeacaa8d1e6f1",
+    "url": "/static/js/main.038f835a.chunk.js"
+  },
+  {
+    "revision": "12dc24d969058aa7bd1f",
+    "url": "/static/js/1.12dc24d9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7b7ec07d9cc12298eb6e0a83e84aee17",
+    "url": "/index.html"
+  }
+];
